7d845d0f9e7c84aa252fa6ab0ad4a79a4173ee96
fullgukbap <2hanjunbum6@gmail.com>
2024-09-23 21:23:16 +0900

    feat: post repository 구현 완료

---
+++ internal/core/port/port_post.go
@@ -0,0 +1,18 @@
+package port
+
+import (
+	"context"
+	"samsamoohooh-go-api/internal/core/domain"
+)
+
+type PostRepository interface {
+	Create(ctx context.Context, post *domain.Post) (*domain.Post, error)
+
+	GetByID(ctx context.Context, id uint) (*domain.Post, error)
+	GetCommentsByID(ctx context.Context, id uint) ([]*domain.Comment, error)
+	GetAll(ctx context.Context, skip, limit int) ([]*domain.Post, error)
+
+	Update(ctx context.Context, id uint, user *domain.Post) (*domain.Post, error)
+
+	Delete(ctx context.Context, id uint) error
+}
