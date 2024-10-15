f94e61c2e3c8d00cb2ab45bbd93f34df4e6244a9
fullgukbap <2hanjunbum6@gmail.com>
2024-10-16 08:42:38 +0900

    feat: user repository 작성 완료 및 예시 작성

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
-	"fmt"
+	"github.com/gofiber/fiber/v2"
 	"log"
 	"samsamoohooh-go-api/internal/domain"
+	"samsamoohooh-go-api/internal/handler"
+	"samsamoohooh-go-api/internal/infra/catcher"
 	"samsamoohooh-go-api/internal/infra/config"
+	"samsamoohooh-go-api/internal/infra/middleware"
 	"samsamoohooh-go-api/internal/infra/token"
+	"samsamoohooh-go-api/internal/repository"
+	"samsamoohooh-go-api/internal/repository/database"
+	"samsamoohooh-go-api/internal/service"
 )
 
 func main() {
@@ -14,37 +20,38 @@ func main() {
 		log.Panicf("failed to load config: %v\n", err)
 	}
 
+	db, err := database.NewDatabase(cfg)
+	if err != nil {
+		log.Panicf("failed to connect to database: %v\n", err)
+	}
+	defer func() {
+		err := db.Close()
+		if err != nil {
+			log.Panicf("failed to close database connection: %v\n", err)
+		}
+	}()
+
+	userRepository := repository.NewUserRepository(db)
+	userService := service.NewUserService(userRepository)
+	userHandler := handler.NewUserHandler(userService)
+
 	var tokenService domain.TokenService = token.NewJWTService(cfg)
+	tokenMiddleware := middleware.NewTokenMiddleware(tokenService)
 
-	accessTokenString, err := tokenService.GenerateAccessTokenString(1, domain.TokenRoleGuest)
-	if err != nil {
-		log.Fatalf("failed to generate access token string: %v\n", err)
+	app := fiber.New(fiber.Config{
+		ErrorHandler: catcher.ErrorHandler,
+	})
+	v1 := app.Group("v1")
+	{
+		api := v1.Group("/api")
+		{
+			useMiddleware := api.Group("", tokenMiddleware.Authorization)
+			{
+				userHandler.Route(useMiddleware)
+			}
+		}
 	}
 
-	refreshTokenString, err := tokenService.GenerateRefreshTokenString(1, domain.TokenRoleGuest)
-	if err != nil {
-		log.Fatalf("failed to generate refresh token string: %v\n", err)
-	}
+	log.Fatal(app.Listen(":8080"))
 
-	accessToken, err := tokenService.ParseToken(accessTokenString)
-	if err != nil {
-		log.Fatalf("failed to parse access token: %v\n", err)
-	}
-
-	accessTokenIsValid, err := tokenService.ValidateToken(accessTokenString)
-	if err != nil {
-		log.Fatalf("failed to validate access token: %v\n", err)
-	}
-
-	refreshToken, err := tokenService.ParseToken(refreshTokenString)
-	if err != nil {
-		log.Fatalf("failed to parse refresh token: %v\n", err)
-	}
-
-	refreshTokenIsValid, err := tokenService.ValidateToken(refreshTokenString)
-	if err != nil {
-		log.Fatalf("failed to validate refresh token: %v\n", err)
-	}
-
-	fmt.Printf("parsed access token: %+v isValid: %v\nparsed refresh token: %+v isValid: %v\n", accessToken, accessTokenIsValid, refreshToken, refreshTokenIsValid)
 }
