5bd9d3472253917ef1e3d3ec833583677c3204bc
fullgukbap <2hanjunbum6@gmail.com>
2024-10-15 19:52:36 +0900

    feat: repository error handler 할 수 있게 함

--- internal/domain/domain_errors.go
+++ internal/domain/domain_errors.go
@@ -8,4 +8,10 @@ var (
 	ErrInvalidTokenIssuer = errors.New("invalid token issuer")
 	ErrTokenNotActiveYet  = errors.New("token is not active yet")
 	ErrTokenExpired       = errors.New("token is expired")
+
+	ErrNotFound    = errors.New("not found")
+	ErrNotLoaded   = errors.New("not loaded")
+	ErrConstraint  = errors.New("constraint error")
+	ErrNotSingular = errors.New("singular error")
+	ErrValidation  = errors.New("validation error")
 )
--- internal/infra/catcher/catcher.go
+++ internal/infra/catcher/catcher.go
@@ -1,4 +1,4 @@
-package catch
+package catcher
 
 import (
 	"github.com/gofiber/fiber/v2"
@@ -24,6 +24,18 @@ var ErrorHandler = func(c *fiber.Ctx, caughtErr error) error {
 
 	case errors.Is(domain.ErrTokenExpired, caughtErr):
 		status = fiber.StatusUnauthorized
+
+	case errors.Is(domain.ErrNotFound, caughtErr):
+		status = fiber.StatusNotFound
+	case errors.Is(domain.ErrNotLoaded, caughtErr):
+		status = fiber.StatusBadRequest
+	case errors.Is(domain.ErrConstraint, caughtErr):
+		status = fiber.StatusConflict
+	case errors.Is(domain.ErrNotSingular, caughtErr):
+		status = fiber.StatusBadRequest
+
+	case errors.Is(domain.ErrValidation, caughtErr):
+		status = fiber.StatusBadRequest
 
 	default:
 		return fiber.DefaultErrorHandler(c, caughtErr)
