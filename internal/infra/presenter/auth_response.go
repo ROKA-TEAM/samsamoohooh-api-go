ef75b6a94171e37b7b0e81549c5100c1677b9b39
fullgukbap <2hanjunbum6@gmail.com>
2024-10-17 10:25:44 +0900

    feat: handler request에 대응하기 위한 presenter 구현 완료

--- internal/infra/presenter/auth_response.go
+++ internal/infra/presenter/auth_response.go
@@ -9,3 +9,7 @@ type KaKaoCallbackResponse struct {
 	AccessToken  string `json:"access_token"`
 	RefreshToken string `json:"refresh_token"`
 }
+
+type AuthRefreshResponse struct {
+	AccessToken string `json:"accessToken"`
+}
