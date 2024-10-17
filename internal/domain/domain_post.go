629411094f6223adf0303898473a3defe8340fe6
fullgukbap <2hanjunbum6@gmail.com>
2024-10-17 17:08:04 +0900

    feat: post repository, service interface 정의

--- internal/domain/domain_post.go
+++ internal/domain/domain_post.go
@@ -1,6 +1,9 @@
 package domain
 
-import "time"
+import (
+	"context"
+	"time"
+)
 
 type Post struct {
 	ID      int
@@ -16,4 +19,22 @@ type Post struct {
 
 	UserID  int
 	GroupID int
+}
+
+type PostRepository interface {
+	Create(ctx context.Context, post *Post) (*Post, error)
+	List(ctx context.Context, offset, limit int) ([]*Post, error)
+	GetByID(ctx context.Context, id int) (*Post, error)
+	GetCommentsByID(ctx context.Context, id int) ([]*Comment, error)
+	Update(ctx context.Context, id int, post *Post) (*Post, error)
+	Delete(ctx context.Context, id int) error
+}
+
+type PostService interface {
+	Create(ctx context.Context, post *Post) (*Post, error)
+	List(ctx context.Context, offset, limit int) ([]*Post, error)
+	GetByID(ctx context.Context, id int) (*Post, error)
+	GetCommentsByID(ctx context.Context, id int) ([]*Comment, error)
+	Update(ctx context.Context, id int, post *Post) (*Post, error)
+	Delete(ctx context.Context, id int) error
 }
