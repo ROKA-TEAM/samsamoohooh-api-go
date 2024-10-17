d47901ce28e63c6cb7204bfc6060711e7603c975
fullgukbap <2hanjunbum6@gmail.com>
2024-10-17 14:08:17 +0900

    feat: group repository 구현 완료

--- internal/domain/domain_group.go
+++ internal/domain/domain_group.go
@@ -1,6 +1,9 @@
 package domain
 
-import "time"
+import (
+	"context"
+	"time"
+)
 
 type Group struct {
 	ID          int
@@ -18,4 +21,26 @@ type Group struct {
 	Users  []*User
 	Tasks  []*Task
 	Groups []*Group
+}
+
+type GroupRepository interface {
+	Create(ctx context.Context, group *Group) (*Group, error)
+	List(ctx context.Context, offset, limit int) ([]*Group, error)
+	GetByID(ctx context.Context, id int) (*Group, error)
+	GetUsersByID(ctx context.Context, id int, offset, limit int) ([]*User, error)
+	GetPostsByID(ctx context.Context, id int, offset, limit int) ([]*Post, error)
+	GetTasksByID(ctx context.Context, id int, offset, limit int) ([]*Task, error)
+	Update(ctx context.Context, id int, group *Group) (*Group, error)
+	Delete(ctx context.Context, id int) error
+}
+
+type GruopService interface {
+	Create(ctx context.Context, group *Group) (*Group, error)
+	List(ctx context.Context, offset, limit int) ([]*Group, error)
+	GetByID(ctx context.Context, id int) (*Group, error)
+	GetUsersByID(ctx context.Context, id int, offset, limit int) ([]*User, error)
+	GetPostsByID(ctx context.Context, id int, offset, limit int) ([]*Post, error)
+	GetTasksByID(ctx context.Context, id int, offset, limit int) ([]*Task, error)
+	Update(ctx context.Context, id int, group *Group) (*Group, error)
+	Delete(ctx context.Context, id int) error
 }
