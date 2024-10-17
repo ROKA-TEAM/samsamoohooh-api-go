46ee0594c1b0770b53c14b655b8d4d1f62db1bab
fullgukbap <2hanjunbum6@gmail.com>
2024-10-17 14:52:31 +0900

    feat: user handler 구현 완료

--- internal/handler/handler_group.go
+++ internal/handler/handler_group.go
@@ -2,6 +2,8 @@ package handler
 
 import (
 	"samsamoohooh-go-api/internal/domain"
+	"samsamoohooh-go-api/internal/handler/utils"
+	"samsamoohooh-go-api/internal/infra/presenter"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -17,9 +19,133 @@ func NewGroupHandler(groupService domain.GroupService) *GroupHandler {
 }
 
 func (h *GroupHandler) Route(router fiber.Router) {
-
+	router.Post("/", h.Create)
+	router.Get("/", h.List)
+	router.Get("/:id", h.GetByID)
+	router.Get("/:id/users", h.GetUsersByID)
+	router.Get("/:id/posts", h.GetPostsByID)
+	router.Get("/:id/tasks", h.GetTasksByID)
+	router.Put("/:id", h.Update)
+	router.Delete("/:id", h.Delete)
 }
 
 func (h *GroupHandler) Create(c *fiber.Ctx) error {
-	return nil
+	body := new(presenter.GroupCreateReqeust)
+	if err := utils.ParseAndVerify(c, body); err != nil {
+		return err
+	}
+
+	createdGroup, err := h.groupService.Create(c.Context(), body.ToDomain())
+	if err != nil {
+		return err
+	}
+
+	return c.Status(fiber.StatusCreated).JSON(presenter.NewGroupCreateResponse(createdGroup))
+}
+
+func (h *GroupHandler) List(c *fiber.Ctx) error {
+	limit := c.QueryInt("limit", DefaultLimit)
+	offset := c.QueryInt("offset", DefaultOffset)
+
+	groups, err := h.groupService.List(c.Context(), limit, offset)
+	if err != nil {
+		return err
+	}
+
+	return c.Status(fiber.StatusOK).JSON(presenter.NewGroupListResponse(groups))
+}
+
+func (h *GroupHandler) GetByID(c *fiber.Ctx) error {
+	id, err := c.ParamsInt("id")
+	if err != nil {
+		return err
+	}
+
+	gotGroup, err := h.groupService.GetByID(c.Context(), id)
+	if err != nil {
+		return err
+	}
+
+	return c.Status(fiber.StatusOK).JSON(presenter.NewGroupGetByIDResponse(gotGroup))
+}
+
+func (h *GroupHandler) GetUsersByID(c *fiber.Ctx) error {
+	id, err := c.ParamsInt("id")
+	if err != nil {
+		return err
+	}
+	limit := c.QueryInt("limit", DefaultLimit)
+	offset := c.QueryInt("offset", DefaultOffset)
+
+	users, err := h.groupService.GetUsersByID(c.Context(), id, offset, limit)
+	if err != nil {
+		return err
+	}
+
+	return c.Status(fiber.StatusOK).JSON(presenter.NewGroupGetUsersByIDResponse(users))
+}
+
+func (h *GroupHandler) GetPostsByID(c *fiber.Ctx) error {
+	id, err := c.ParamsInt("id")
+	if err != nil {
+		return err
+	}
+	limit := c.QueryInt("limit", DefaultLimit)
+	offset := c.QueryInt("offset", DefaultOffset)
+
+	listPost, err := h.groupService.GetPostsByID(c.Context(), id, offset, limit)
+	if err != nil {
+		return err
+	}
+
+	return c.Status(fiber.StatusOK).JSON(presenter.NewGroupGetPostsByIDResponse(listPost))
+}
+
+func (h *GroupHandler) GetTasksByID(c *fiber.Ctx) error {
+	id, err := c.ParamsInt("id")
+	if err != nil {
+		return err
+	}
+	limit := c.QueryInt("limit", DefaultLimit)
+	offset := c.QueryInt("offset", DefaultOffset)
+
+	listTask, err := h.groupService.GetTasksByID(c.Context(), id, offset, limit)
+	if err != nil {
+		return err
+	}
+
+	return c.Status(fiber.StatusOK).JSON(presenter.NewGroupGetTasksByIDResponse(listTask))
+}
+
+func (h *GroupHandler) Update(c *fiber.Ctx) error {
+	id, err := c.ParamsInt("id")
+	if err != nil {
+		return err
+	}
+
+	body := new(presenter.GroupUpdateRequest)
+	if err := utils.ParseAndVerify(c, body); err != nil {
+		return err
+	}
+
+	updatedGroup, err := h.groupService.Update(c.Context(), id, body.ToDomain())
+	if err != nil {
+		return err
+	}
+
+	return c.Status(fiber.StatusOK).JSON(presenter.NewGroupUpdateResponse(updatedGroup))
+}
+
+func (h *GroupHandler) Delete(c *fiber.Ctx) error {
+	id, err := c.ParamsInt("id")
+	if err != nil {
+		return err
+	}
+
+	err = h.groupService.Delete(c.Context(), id)
+	if err != nil {
+		return err
+	}
+
+	return c.SendStatus(fiber.StatusNoContent)
 }
