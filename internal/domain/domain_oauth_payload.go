ff52218046e163b9b65d222dae23aa3c4111564e
fullgukbap <2hanjunbum6@gmail.com>
2024-10-17 09:25:31 +0900

    feat: oauth code grant type service 추가

--- internal/domain/domain_oauth_payload.go
+++ internal/domain/domain_oauth_payload.go
@@ -1,6 +1,14 @@
 package domain
 
+import "context"
+
 type OauthPayload struct {
 	Name string
 	Sub  string
+}
+
+type OauthAuthorizationGrantService interface {
+	GetLoginURL(state string) string
+	Exchange(ctx context.Context, code string) (*OauthPayload, error)
+	AuthenticateOrRegister(ctx context.Context, code string) (string, string, error)
 }
