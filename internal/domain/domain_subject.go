86a0c4fec4503c0c3821b4ae4bfdbc40332a7896
fullgukbap <2hanjunbum6@gmail.com>
2024-10-18 11:56:57 +0900

    fix: timestamp 추가 못해서 지금 추가

--- internal/domain/domain_subject.go
+++ internal/domain/domain_subject.go
@@ -1,9 +1,15 @@
 package domain
 
+import "time"
+
 type Subject struct {
-	ID      int
-	Topic   string
-	Feeling string
-	UserID  int
-	TaskID  int
+	ID        int
+	Topic     string
+	Feeling   string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt time.Time
+
+	UserID int
+	TaskID int
 }
