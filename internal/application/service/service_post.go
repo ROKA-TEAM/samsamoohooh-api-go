839901deef9ee6cdb6bd406ad73ebd3cf588e8d1
fullgukbap <2hanjunbum6@gmail.com>
2024-10-21 10:24:30 +0900

    refactor: post service 다시 구현

--- internal/application/service/service_post.go
+++ internal/application/service/service_post.go
@@ -2,39 +2,71 @@ package service
 
 import (
 	"context"
-	domain2 "samsamoohooh-go-api/internal/application/domain"
+	domain "samsamoohooh-go-api/internal/application/domain"
 )
 
-var _ domain2.PostService = (*PostService)(nil)
+var _ domain.PostService = (*PostService)(nil)
 
 type PostService struct {
-	postRepository domain2.PostRepository
+	postRepository domain.PostRepository
 }
 
-func NewPostService(postRepository domain2.PostRepository) *PostService {
+func NewPostService(
+	postRepository domain.PostRepository,
+) *PostService {
 	return &PostService{postRepository: postRepository}
 }
 
-func (p *PostService) Create(ctx context.Context, groupID int, post *domain2.Post) (*domain2.Post, error) {
-	return p.postRepository.Create(ctx, groupID, post)
+func (p *PostService) CreatePost(ctx context.Context, groupID int, post *domain.Post) (*domain.Post, error) {
+	createdPost, err := p.postRepository.CreatePost(ctx, groupID, post)
+	if err != nil {
+		return nil, err
+	}
+
+	return createdPost, nil
 }
 
-func (p *PostService) List(ctx context.Context, offset, limit int) ([]*domain2.Post, error) {
-	return p.postRepository.List(ctx, offset, limit)
+func (p *PostService) GetPosts(ctx context.Context, offset, limit int) ([]*domain.Post, error) {
+	listPost, err := p.postRepository.GetPosts(ctx, offset, limit)
+	if err != nil {
+		return nil, err
+	}
+
+	return listPost, nil
 }
 
-func (p *PostService) GetByID(ctx context.Context, id int) (*domain2.Post, error) {
-	return p.postRepository.GetByID(ctx, id)
+func (p *PostService) GetByPostID(ctx context.Context, id int) (*domain.Post, error) {
+	gotPost, err := p.postRepository.GetByPostID(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+
+	return gotPost, nil
 }
 
-func (p *PostService) GetCommentsByID(ctx context.Context, id, offset, limit int) ([]*domain2.Comment, error) {
-	return p.postRepository.GetCommentsByID(ctx, id, offset, limit)
+func (p *PostService) GetCommentsByPostID(ctx context.Context, id, offset, limit int) ([]*domain.Comment, error) {
+	listComment, err := p.postRepository.GetCommentsByPostID(ctx, id, offset, limit)
+	if err != nil {
+		return nil, err
+	}
+
+	return listComment, nil
 }
 
-func (p *PostService) Update(ctx context.Context, id int, post *domain2.Post) (*domain2.Post, error) {
-	return p.postRepository.Update(ctx, id, post)
+func (p *PostService) UpdatePost(ctx context.Context, id int, post *domain.Post) (*domain.Post, error) {
+	updatedPost, err := p.postRepository.UpdatePost(ctx, id, post)
+	if err != nil {
+		return nil, err
+	}
+
+	return updatedPost, nil
 }
 
-func (p *PostService) Delete(ctx context.Context, id int) error {
-	return p.postRepository.Delete(ctx, id)
+func (p *PostService) DeletePost(ctx context.Context, id int) error {
+	err := p.postRepository.DeletePost(ctx, id)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
