78d50898c52984e6829724d58ac8135a76375dbd
fullgukbap <2hanjunbum6@gmail.com>
2024-10-21 10:16:18 +0900

    refactor: topic servie 리펙토링

--- internal/application/service/service_topic.go
+++ internal/application/service/service_topic.go
@@ -5,26 +5,59 @@ import (
 	"samsamoohooh-go-api/internal/application/domain"
 )
 
+var _ domain.TopicService = (*TopicService)(nil)
+
 type TopicService struct {
 	topicRepository domain.TopicRepository
 }
 
-func NewTopicService(topicRepository domain.TopicRepository) *TopicService {
+func NewTopicService(
+	topicRepository domain.TopicRepository,
+) *TopicService {
 	return &TopicService{topicRepository: topicRepository}
 }
 
-func (s *TopicService) Create(ctx context.Context, taskID int, topic *domain.Topic) (*domain.Topic, error) {
-	return s.topicRepository.Create(ctx, taskID, topic)
+func (s *TopicService) CreateTopic(ctx context.Context, taskID int, topic *domain.Topic) (*domain.Topic, error) {
+	createdTopic, err := s.topicRepository.CreateTopic(ctx, taskID, topic)
+	if err != nil {
+		return nil, err
+	}
+
+	return createdTopic, nil
 }
-func (s *TopicService) List(ctx context.Context, offset, limit int) ([]*domain.Topic, error) {
-	return s.topicRepository.List(ctx, offset, limit)
+
+func (s *TopicService) GetTopics(ctx context.Context, offset, limit int) ([]*domain.Topic, error) {
+	listTopic, err := s.topicRepository.GetTopics(ctx, offset, limit)
+	if err != nil {
+		return nil, err
+	}
+
+	return listTopic, nil
 }
-func (s *TopicService) GetByID(ctx context.Context, id int) (*domain.Topic, error) {
-	return s.topicRepository.GetByID(ctx, id)
+func (s *TopicService) GetByTopicID(ctx context.Context, id int) (*domain.Topic, error) {
+	gotTopic, err := s.topicRepository.GetByTopicID(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+
+	return gotTopic, nil
 }
-func (s *TopicService) Update(ctx context.Context, id int, topic *domain.Topic) (*domain.Topic, error) {
-	return s.topicRepository.Update(ctx, id, topic)
+
+func (s *TopicService) UpdateTopic(ctx context.Context, id int, topic *domain.Topic) (*domain.Topic, error) {
+	updatedTopic, err := s.topicRepository.UpdateTopic(ctx, id, topic)
+	if err != nil {
+		return nil, err
+	}
+
+	return updatedTopic, nil
 }
-func (s *TopicService) Delete(ctx context.Context, id int) error {
-	return s.topicRepository.Delete(ctx, id)
+
+func (s *TopicService) DeleteTopic(ctx context.Context, id int) error {
+
+	err := s.topicRepository.DeleteTopic(ctx, id)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
