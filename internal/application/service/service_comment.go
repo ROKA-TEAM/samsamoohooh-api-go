8320c34a2cbcf0712d8e3e417d39842fdb94e636
fullgukbap <2hanjunbum6@gmail.com>
2024-10-21 10:49:31 +0900

    refactor: comment service 리펙토링

--- internal/application/service/service_comment.go
+++ internal/application/service/service_comment.go
@@ -5,30 +5,59 @@ import (
 	"samsamoohooh-go-api/internal/application/domain"
 )
 
+var _ domain.CommentRepository = (*CommentService)(nil)
+
 type CommentService struct {
 	commentRepository domain.CommentRepository
 }
 
-func NewCommentService(commentRepository domain.CommentRepository) *CommentService {
+func NewCommentService(
+	commentRepository domain.CommentRepository,
+) *CommentService {
 	return &CommentService{commentRepository: commentRepository}
 }
 
-func (s *CommentService) Create(ctx context.Context, postID int, comment *domain.Comment) (*domain.Comment, error) {
-	return s.commentRepository.Create(ctx, postID, comment)
+func (s *CommentService) CreateComment(ctx context.Context, postID int, comment *domain.Comment) (*domain.Comment, error) {
+	createdComment, err := s.commentRepository.CreateComment(ctx, postID, comment)
+	if err != nil {
+		return nil, err
+	}
+
+	return createdComment, nil
 }
 
-func (s *CommentService) List(ctx context.Context, offset, limit int) ([]*domain.Comment, error) {
-	return s.commentRepository.List(ctx, offset, limit)
+func (s *CommentService) GetComments(ctx context.Context, offset, limit int) ([]*domain.Comment, error) {
+	gotComment, err := s.commentRepository.GetComments(ctx, offset, limit)
+	if err != nil {
+		return nil, err
+	}
+
+	return gotComment, nil
 }
 
-func (s *CommentService) GetByID(ctx context.Context, id int) (*domain.Comment, error) {
-	return s.commentRepository.GetByID(ctx, id)
+func (s *CommentService) GetByCommentID(ctx context.Context, id int) (*domain.Comment, error) {
+	gotComment, err := s.commentRepository.GetByCommentID(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+
+	return gotComment, nil
 }
 
-func (s *CommentService) Update(ctx context.Context, id int, comment *domain.Comment) (*domain.Comment, error) {
-	return s.commentRepository.Update(ctx, id, comment)
+func (s *CommentService) UpdateComment(ctx context.Context, id int, comment *domain.Comment) (*domain.Comment, error) {
+	updatedComment, err := s.commentRepository.UpdateComment(ctx, id, comment)
+	if err != nil {
+		return nil, err
+	}
+
+	return updatedComment, nil
 }
 
-func (s *CommentService) Delete(ctx context.Context, id int) error {
-	return s.commentRepository.Delete(ctx, id)
+func (s *CommentService) DeleteComment(ctx context.Context, id int) error {
+	err := s.commentRepository.DeleteComment(ctx, id)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
