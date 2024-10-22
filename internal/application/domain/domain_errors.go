1fb0a4704008ae74177bc5b3915b094aa3df7f9e
fullgukbap <2hanjunbum6@gmail.com>
2024-10-22 14:16:34 +0900

    feat: domain 추가

--- internal/application/domain/domain_errors.go
+++ internal/application/domain/domain_errors.go
@@ -7,9 +7,11 @@ import (
 
 var (
 	// database errors
-	ErrConstraint  = box.NewError("constraint violation", http.StatusBadRequest)
-	ErrNotFound    = box.NewError("not found", http.StatusNotFound)
-	ErrNotLoaded   = box.NewError("not loaded", http.StatusNotFound)
-	ErrNotSingular = box.NewError("not singular", http.StatusNotFound)
-	ErrValidation  = box.NewError("validation error", http.StatusBadRequest)
+	ErrConstraint    = box.NewError("constraint violation", http.StatusBadRequest)
+	ErrNotFound      = box.NewError("not found", http.StatusNotFound)
+	ErrNotLoaded     = box.NewError("not loaded", http.StatusNotFound)
+	ErrNotSingular   = box.NewError("not singular", http.StatusNotFound)
+	ErrValidation    = box.NewError("validation error", http.StatusBadRequest)
+	ErrAuthorization = box.NewError("authorization error", http.StatusUnauthorized)
+	ErrForbidden     = box.NewError("forbidden", http.StatusForbidden)
 )
