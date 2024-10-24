6ff5ee7c41f9fb13565a19ce82eda65af37a6145
fullgukbap <2hanjunbum6@gmail.com>
2024-10-24 21:27:51 +0900

    feat: exception 패키지 추가

---
+++ internal/infra/exception/exception.go
@@ -0,0 +1,54 @@
+package exception
+
+type Exception struct {
+	Type    string
+	Status  int
+	Message string
+	Data    any
+
+	Err error
+}
+
+func New(err error, _type string, status int, message string, opts ...ExceptionOption) *Exception {
+	exception := &Exception{
+		Err:     err,
+		Type:    _type,
+		Status:  status,
+		Message: message,
+	}
+
+	// 옵션 적용
+	for _, opt := range opts {
+		opt(exception)
+	}
+
+	return exception
+}
+
+func NewWithoutErr(_type string, status int, message string, opts ...ExceptionOption) *Exception {
+	exception := &Exception{
+		Type:    _type,
+		Status:  status,
+		Message: message,
+	}
+
+	// 옵션 적용
+	for _, opt := range opts {
+		opt(exception)
+	}
+
+	return exception
+}
+
+type Map map[string]any
+type ExceptionOption func(*Exception)
+
+func WithData(data any) ExceptionOption {
+	return func(e *Exception) {
+		e.Data = data
+	}
+}
+
+func (e Exception) Error() string {
+	return e.Err.Error()
+}
