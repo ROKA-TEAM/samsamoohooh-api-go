9d714eee19723b9e1458fdb422c4809ce43d553b
fullgukbap <2hanjunbum6@gmail.com>
2024-10-27 15:31:33 +0900

    feat: database migration 코드 추가

--- cmd/app/app.go
+++ cmd/app/app.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
+	"log"
 	"samsamoohooh-go-api/internal/application"
 	"samsamoohooh-go-api/internal/infra"
+	"samsamoohooh-go-api/internal/infra/storage/mysql"
 	"samsamoohooh-go-api/internal/router"
+	"time"
 
 	"go.uber.org/fx"
 )
@@ -25,6 +29,16 @@ func main() {
 		application.ServiceModule,
 		application.HandlerModule,
 
-		fx.Invoke(func(r *router.Router) {}),
+		fx.Invoke(
+			func(r *router.Router) {},
+			func(mysql *mysql.MySQL) {
+				ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+				defer cancel()
+
+				if err := mysql.AutoMigration(ctx); err != nil {
+					log.Panicf("failed to migrate: %v", err)
+				}
+			},
+		),
 	).Run()
 }
